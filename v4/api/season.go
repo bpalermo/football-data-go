bfc91e62216ae4c776da95cf8e4a50ec484c8a30
Bruno Palermo <b@palermo.dev>
2022-07-17 18:13:16 -0300

    Add support for DynamoDB attribute value (#3)

--- v4/api/season.go
+++ v4/api/season.go
@@ -1,9 +1,9 @@
 package api
 
 type Season struct {
-	Id              uint64   `json:"id"`
-	StartDate       *ISODate `json:"startDate,omitempty"`
-	EndDate         *ISODate `json:"endDate,omitempty"`
-	CurrentMatchDay uint8    `json:"currentMatchDay"`
-	Winner          *Team    `json:"winner,omitempty"`
+	Id              uint64   `json:"id" dynamodbav:"id"`
+	StartDate       *ISODate `json:"startDate,omitempty" dynamodbav:"startDate"`
+	EndDate         *ISODate `json:"endDate,omitempty" dynamodbav:"endDate"`
+	CurrentMatchDay uint8    `json:"currentMatchDay" dynamodbav:"currentMatchDay"`
+	Winner          *Team    `json:"winner,omitempty" dynamodbav:"winner"`
 }
